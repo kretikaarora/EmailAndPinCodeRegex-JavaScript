97d3efd28f1188fa122b5e179d24e213e5e74bc0
kretika arora <kretikaarora@gmail.com>
2020-11-19 20:35:38 +0530

    [KretikaArora]Validating Email third mandatory Part using regex

--- EmailRegex.js
+++ EmailRegex.js
@@ -1,7 +1,8 @@
 //email format- abc.xyz@bridgelabz.co.in .xyz & .in optional & rest all mandatory
 //uc1 validating first part of the email i.e. abc
 //uc2 changing email pattern for validating second part i.e. @bridgelabs
-let emailRegex=RegExp("^[A-Za-z]{3,}[@][A-Za-z0-9]{1,}$")
+//uc3 validating third part .co in email pattern
+let emailRegex=RegExp("^[A-Za-z]{3,}[@][A-Za-z0-9]{1,}[.][a-zA-Z0-9]{2,3}$")
 function EmailValidation(email)
 {
     if(emailRegex.test(email))
