18a6eb8268bacfa5e5e526bba293bff0f558c929
kretika arora <kretikaarora@gmail.com>
2020-11-19 20:31:14 +0530

    [KretikaArora]Validating Email Second Part using regex

--- EmailRegex.js
+++ EmailRegex.js
@@ -1,5 +1,7 @@
-//uc1 validating first part of the email 
-let emailRegex=RegExp("^[A-Za-z]{3,}$")
+//email format- abc.xyz@bridgelabz.co.in .xyz & .in optional & rest all mandatory
+//uc1 validating first part of the email i.e. abc
+//uc2 changing email pattern for validating second part i.e. @bridgelabs
+let emailRegex=RegExp("^[A-Za-z]{3,}[@][A-Za-z0-9]{1,}$")
 function EmailValidation(email)
 {
     if(emailRegex.test(email))
